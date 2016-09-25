9086a81adfc4051cd36f6e7aa182bdcc0cc86cee
megaarman <arman1@vt.edu>
2016-09-24 22:02:44 -0400

    Removed not essential files

--- JS_options/xml2js_test/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-//MAIN CLASS
-//NOTE: require should work in browser assuming you have browserify installed
-var greetings = require("./boilerplate.js");
-// greetings.helloWorld(); //prints Hello World! if you have browserify setup correctly
-//=============================================
