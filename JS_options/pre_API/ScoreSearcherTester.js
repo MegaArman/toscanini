ab394fe73af5a60c7bf1d3cbd35def73ee46c1e9
megaarman <arman1@vt.edu>
2016-10-09 00:47:32 -0400

    Outputted execution time test using nodes hrtime

--- JS_options/pre_API/ScoreSearcherTester.js
+++ JS_options/pre_API/ScoreSearcherTester.js
@@ -1,15 +1,18 @@
 //Currently the 'import' statement is not widely supported, hence 'require' suffices
+var hrstart = process.hrtime(); //node supported and more precise for speed test
+
 let fs = require('fs'),
     xml2js = require('xml2js');
 
 let parser = new xml2js.Parser({explicitArray: false, mergeAttrs: true});
 //no explicitArray: [ 'Violin', 'Flute' ] , with explicitArray: [ [ 'Violin' ], [ 'Flute' ] ]
 //merge attributes treats atrributes the same as child elements
-//no explicitArray and mergeAttrs reduce the object size 
+//no explicitArray and mergeAttrs reduce the object size
 
 let ScoreSearcher = require('./ScoreSearcher.js');
 
-// fs.readFile('./avamariapg1.xml', function(err, data) {     //data is the whole score as a string!!!
+//data is the whole score as a string!!!
+// fs.readFile('./avamariapg1.xml', function(err, data) {
 //     let basicPieceRaw;
 //
 //     parser.parseString(data, function (err, result) {
@@ -21,11 +24,30 @@ let ScoreSearcher = require('./ScoreSearcher.js');
 //   });
 // });
 
-fs.readFile('./two_parts_simple.xml', function(err, data) {     //data is the whole score as a string!!!
+// fs.readFile('./two_parts_simple.xml', function(err, data) {     !
+//     let basicPieceRaw;
+//
+//     parser.parseString(data, function (err, result) {
+//       basicPieceRaw = result;
+//       const basicPiece = new ScoreSearcher(basicPieceRaw);
+//   });
+// });
+
+//===============================================================
+//PERFORMANCE TEST:
+//winter is 3.8mb
+fs.readFile('./vivaldi_winter.xml', function(err, data) {
     let basicPieceRaw;
 
     parser.parseString(data, function (err, result) {
       basicPieceRaw = result;
       const basicPiece = new ScoreSearcher(basicPieceRaw);
+      // basicPiece.findValsByKey('instrument-name');
+      // console.log('max midi pitch is ' + basicPiece.getMaxPitch());
+      // console.log('min midi pitch is ' + basicPiece.getMinPitch());
+
+      //PRINT EXECUTION TIME :
+      var hrend = process.hrtime(hrstart);
+      console.log("Execution time (hr): ", hrend[0], hrend[1]/1000000);
   });
 });
