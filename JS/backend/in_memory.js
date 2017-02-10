54a3e08321b8080f4fb6f5c746aeb82dde3d537b
MegaArman <arman1@vt.edu>
2017-02-09 21:01:42 -0500

    updated in_memory.js

--- JS/backend/in_memory.js
+++ JS/backend/in_memory.js
@@ -2,16 +2,20 @@
 const fs = require("fs");
 const ScoreSearcher = require("./ScoreSearcher");
 
-const jsObj1 = fs.readFileSync("../scores_json/Beethoven-Symphony No. 4 Mov. 1.json");
-const jsObj2 = fs.readFileSync("../scores_json/Beethoven-Symphony No. 4 Mov. 4.json");
+const xml1 = fs.readFileSync("../scores_json/Beethoven-Symphony No. 4 Mov. 1.json");
+const xml2 = fs.readFileSync("../scores_json/Beethoven-Symphony No. 4 Mov. 4.json");
+const jsObj1 = JSON.parse(xml1);
+const jsObj2 = JSON.parse(xml2);
 
 function run()
 {
   console.time("");
-  for (let i = 0; i < 5; i++)
+  for (let i = 0; i < 100; i++)
   {
-    let scoreSearcher1 = ScoreSearcher(JSON.parse(jsObj1));
-    let scoreSearcher2 = ScoreSearcher(JSON.parse(jsObj2));
+    let scoreSearcher1 = ScoreSearcher(jsObj1);
+    // scoreSearcher1.getMaxPitch();
+    let scoreSearcher2 = ScoreSearcher(jsObj2);
+    // scoreSearcher2.getMaxPitch();
   }
 
   console.timeEnd("");
