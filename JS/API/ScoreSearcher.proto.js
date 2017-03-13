4691b590ce757a53b2bae750bc5a65175417deff
Matthew Fishman <feesh96@vt.edu>
2017-03-12 20:57:49 -0400

    Added getAccidentals function: returns amount of notes not in the current key

--- JS/API/ScoreSearcher.proto.js
+++ JS/API/ScoreSearcher.proto.js
@@ -62,10 +62,35 @@ const factoryScoreSearcher = (musicObj) =>
   //"private" variables..note state is safest kept constant-------------------
   const scoreSearcher = {};
   const instrumentObjects = makeInstrumentObjects(musicObj);
+  let currKey = {"C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "A": 0, "B": 0}; //Sharps/flats for a given note
 
   //"private" functions in scope------------------------------
-
   //...ex: function poop() { ... }
+  function setKey(fifths)
+  {
+    let keyArray = [0, 0, 0, 0, 0, 0, 0]; //[F, C, G, D, A, E, B]
+    if (fifths > 0)   //Sharps
+    {
+      for (let i = 0; i < fifths; i++)
+      {
+        keyArray[i % 7]++;
+      }
+    }
+    else if (fifths < 0)   //Flats
+    {
+      for (let i = 0; i < (-fifths); i++)
+      {
+        keyArray[6 - (i % 7)]--;
+      }
+    }
+    currKey["F"] = keyArray[0];
+    currKey["C"] = keyArray[1];
+    currKey["G"] = keyArray[2];
+    currKey["D"] = keyArray[3];
+    currKey["A"] = keyArray[4];
+    currKey["E"] = keyArray[5];
+    currKey["B"] = keyArray[6];
+  }
 
   //"public" functions---------------------------
   scoreSearcher.findValsByKey = (targetKey) =>
@@ -202,6 +227,25 @@ const factoryScoreSearcher = (musicObj) =>
 
     traverse(musicObj, process);
     return tempos;
+  };
+
+  scoreSearcher.getAccidentals = () =>
+  {
+    let accidentals = 0;
+    let currNote = "A";
+
+    function process(key, value)
+    {
+      if (key === "fifths")
+        setKey(value);
+      else if (key === "step")
+        currNote = value;
+      else if (key === "alter" && currKey[currNote] !== parseInt(value))
+        accidentals++;
+    }
+
+    traverse(musicObj, process);
+    return accidentals;
   };
 
   return scoreSearcher;
