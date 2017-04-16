e71b71657b76909130d251206ad355a3d1f9c86c
Arman Bahraini <arman@mickey.cs.vt.edu>
2017-04-16 04:43:17 -0400

    server works for searching scores and returning results

--- JS/server/search.js
+++ JS/server/search.js
@@ -3,30 +3,23 @@ const http = require("http");
 const fs = require("fs");
 const ScoreSearcher = require("./ScoreSearcher.js");
 
+const musicXML = fs.readFileSync("../scores/vivaldi_winter.xml");
 const scoreSearcher = ScoreSearcher(musicXML);
 const minPitch = scoreSearcher.getMinPitch().toString();
 const maxPitch = scoreSearcher.getMaxPitch().toString();
 const instrumentNames = scoreSearcher.getInstrumentNames().toString();
 const result = {};
-let musicXML;
-
-fs.readFile("../scores/vivaldi_winter.xml", (err, contents) =>
-{
-	if (err)
-		throw err;
-	else
-		musicXML = contents;
-});
 
 result["minPitch"] = minPitch;
 result["maxPitch"] = maxPitch;
 result["instrumentNames"] = instrumentNames;
 
-const server = http.createServer((request, response) =>
+function handleHTTP(req, res)
 {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(JSON.stringify(result));
-});
+	res.end(JSON.stringify(result));
+}
+
+const server = http.createServer(handleHTTP);
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
