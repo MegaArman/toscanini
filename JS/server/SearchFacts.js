208448b93921ec4452b8b54eb762c826be4287eb
Arman Bahraini <arman@mickey.cs.vt.edu>
2017-04-17 18:40:37 -0400

    still working on writingFacts -err in xml2js

--- JS/server/SearchFacts.js
+++ JS/server/SearchFacts.js
@@ -1,14 +1,14 @@
 // Load the http module to create an http server.
-const fs = require("fs");
-const ScoreSearcher = require("./ScoreSearcher.js");
-
-const musicXML = fs.readFileSync("../scores/vivaldi_winter.xml");
-const scoreSearcher = ScoreSearcher(musicXML);
-const minPitch = scoreSearcher.getMinPitch().toString();
-const maxPitch = scoreSearcher.getMaxPitch().toString();
-const instrumentNames = scoreSearcher.getInstrumentNames().toString();
-
-result["minPitch"] = minPitch;
-result["maxPitch"] = maxPitch;
-result["instrumentNames"] = instrumentNames;
+//const fs = require("fs");
+//const ScoreSearcher = require("./ScoreSearcher.js");
+//
+//const musicXML = fs.readFileSync("../scores/vivaldi_winter.xml");
+//const scoreSearcher = ScoreSearcher(musicXML);
+//const minPitch = scoreSearcher.getMinPitch().toString();
+//const maxPitch = scoreSearcher.getMaxPitch().toString();
+//const instrumentNames = scoreSearcher.getInstrumentNames().toString();
+//
+//result["minPitch"] = minPitch;
+//result["maxPitch"] = maxPitch;
+//result["instrumentNames"] = instrumentNames;
 
--- JS/server/WriteFacts.js
+++ JS/server/WriteFacts.js
@@ -1,11 +1,31 @@
 // Load the http module to create an http server.
 const fs = require("fs");
-
-const musicXML = fs.readFileSync("../scores/vivaldi_winter.xml");
 const ScoreSearcher = require("./ScoreSearcher.js");
-const scoreSearcher = ScoreSearcher(musicXML);
 
-//GOAL: {"Beethoven": {"flute": {"min": 45, "max": 60"}}}
+//const musicXML = fs.readFileSync("../scores/vivaldi_winter.xml");
+//const scoreSearcher = ScoreSearcher(musicXML);
+//const instrumentNames = scoreSearcher.getInstrumentNames();
+//const facts = {};
+////GOAL: {"Beethoven": {"flute": {"min": 45, "max": 60"}}}
+//instrumentNames.forEach((instrumentName) => 
+//{
+//	let range = {};
+//	range["min"] = scoreSearcher.getMinPitch(instrumentName);
+//	range["max"] = scoreSearcher.getMaxPitch(instrumentName);
+//	console.log("range", range);
+//	facts[instrumentName] = range;
+//});
+
+const fileNames = fs.readdirSync("./scores");
+fileNames.forEach((fileName) =>
+{
+	console.log("./scores/" + fileName);
+	if (!fileName.includes(".xml"))
+		return;
+		const musicXML = fs.readFileSync("./scores/" + fileName);
+		const scoreSearcher = ScoreSearcher(musicXML);
+	//console.log(scoreSearcher.getMaxPitch());
+});
 
 
-
+console.log("done");
