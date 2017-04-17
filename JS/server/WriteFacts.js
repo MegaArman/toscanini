c6a7de82749e2aab6f12bacd9a733ae2eab34593
Arman Bahraini <arman@mickey.cs.vt.edu>
2017-04-17 17:55:04 -0400

    working on WritingFacts

--- JS/server/WriteFacts.js
+++ JS/server/WriteFacts.js
@@ -1,14 +1,11 @@
 // Load the http module to create an http server.
 const fs = require("fs");
-const ScoreSearcher = require("./ScoreSearcher.js");
 
 const musicXML = fs.readFileSync("../scores/vivaldi_winter.xml");
+const ScoreSearcher = require("./ScoreSearcher.js");
 const scoreSearcher = ScoreSearcher(musicXML);
-const minPitch = scoreSearcher.getMinPitch().toString();
-const maxPitch = scoreSearcher.getMaxPitch().toString();
-const instrumentNames = scoreSearcher.getInstrumentNames().toString();
 
-result["minPitch"] = minPitch;
-result["maxPitch"] = maxPitch;
-result["instrumentNames"] = instrumentNames;
+//GOAL: {"Beethoven": {"flute": {"min": 45, "max": 60"}}}
+
+
 
