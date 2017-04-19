a33798012f08f85c8e4d9edfa32fa5b95e63e641
Arman Bahraini <arman@mickey.cs.vt.edu>
2017-04-19 01:38:48 -0400

    can remove chips

--- JS/server/main.js
+++ JS/server/main.js
@@ -23,21 +23,48 @@ $(".chips-placeholder").material_chip(
 
   
  $(".chips").on("chip.add", (e, chip) =>
- {
-   console.log(chip["tag"]);
+ {     
+   console.log("e", e);
    const splitTag = chip["tag"].split(" ");
-   const minPitch = parseInt(splitTag[1]);
-   const maxPitch = parseInt(splitTag[2]);
-   if (minPitch > maxPitch)
-      alert("min pitch should not be greater than the max pitch!");
-  
 
    if (splitTag.length !== 3)
+   {
+      
       alert("all search criteria must be of format, flute G5 G7");
-   
+      $(".chip:last").remove();
+      return;
+   }
+      
+   const instrumentName = splitTag[0].toLowerCase();
+   const minPitch = parseInt(splitTag[1]);
+   const maxPitch = parseInt(splitTag[2]);
+
+   if (instrumentName in query)
+   {
+    alert("this instrument already has a criteria");
+    $(".chip:last").remove();
+    return;
+   }
+
+   if (minPitch > maxPitch)
+   {
+      alert("min pitch should not be greater than the max pitch, please fix!");
+      $(".chip:last").remove();
+      return;
+   }
+ 
    const range = {};
    range["minPitch"] = minPitch;
    range["maxPitch"] = maxPitch;
-   query[splitTag[0]] = range;
-   console.log(query);
+   query[instrumentName] = range;
+   console.log("post insert", query);
   });
+
+$('.chips').on('chip.delete', function(e, chip)
+{
+  const splitTag = chip["tag"].split(" ");
+  const toDelete = splitTag[0].toLowerCase();
+  delete query[toDelete];
+  console.log("post delete", query);
+});
+                  
