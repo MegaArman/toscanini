b0c854c1aa0067bc8b821e84da07887b6f7abbbc
Arman Bahraini <arman@mickey.cs.vt.edu>
2017-04-19 03:24:57 -0400

    fixed searchFacts to check for each query instruments existance and range

--- JS/server/searchFacts.js
+++ JS/server/searchFacts.js
@@ -1,29 +1,52 @@
 const fs = require("fs");
 const factsJSON = fs.readFileSync("./facts.json");
-//const expected = {"flute": {"minPitch": 50, "maxPitch": 80}};
+const factsMap = new Map(JSON.parse(factsJSON));
 
-module.exports = (expected) =>
+//queries of form {"flute": {"minPitch": 50, "maxPitch": 80}};
+module.exports = (query) => //ex: invoked w searchFacts(query)
 {
-	const factsMap = new Map(JSON.parse(factsJSON));
 	const matchingPieces = [];
-
-	factsMap.forEach((value, pieceName) => 
+  const queryInstrumentNames = Object.keys(query);
+  //................value, key
+	factsMap.forEach((pieceFacts, pieceName) => //iterate over facts pieces
 	{
-		//TODO: should be iterating over expected not V
-		for (let instrumentName in value["instrumentRanges"])
+    //see if piece has query instruments and if they're in range
+    //to do so we need to check substrings, so "trumpet in C" passes for query "trumpet"
+    const pieceInstrumentNames = Object.keys(pieceFacts["instrumentRanges"]);
+    //console.log("queryInstrumentNames", queryInstrumentNames);
+     
+    for (let queryInstrumentName of queryInstrumentNames)
 		{
-			if (instrumentName in expected)
-			{
-				const minPitch = value["instrumentRanges"][instrumentName]["minPitch"];
-				const maxPitch = value["instrumentRanges"][instrumentName]["maxPitch"];
-				const expectedMinPitch = expected[instrumentName]["minPitch"];
-				const expectedMaxPitch = expected[instrumentName]["maxPitch"];
+      let equivalentInstrumentName;
+  
+      for (let pieceInstrumentName of pieceInstrumentNames)
+      {
+        if (pieceInstrumentName.includes(queryInstrumentName))
+           equivalentInstrumentName = pieceInstrumentName;
+      }
+      
+       if (equivalentInstrumentName === undefined)
+       {
+         // console.log(pieceName + "does not have ", queryInstrumentName);
+          return false;
+       }
+       
+     //  console.log("equivalentInstrumentName ", equivalentInstrumentName);
+			 const minPitch = pieceFacts["instrumentRanges"][equivalentInstrumentName]["minPitch"];
+			 const maxPitch = pieceFacts["instrumentRanges"][equivalentInstrumentName]["maxPitch"];
+			 const queryMinPitch = query[queryInstrumentName]["minPitch"];
+			 const queryMaxPitch = query[queryInstrumentName]["maxPitch"];
 	
-				if (minPitch > expectedMinPitch && maxPitch < expectedMaxPitch)
-						matchingPieces.push(pieceName);
-			}
-		}
-	});
+			 if (minPitch < queryMinPitch || maxPitch > queryMaxPitch)
+		   {
+         //console.log(pieceName + " range issue");
+         return false
+       }          	
+    }
+    matchingPieces.push(pieceName); //if it made it this far it passes!
+
+  });
+  
 	return matchingPieces;
 }
 
