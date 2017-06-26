4dfaf8ae1167f8a9362172b0b9a6245745ea6808
galinabee <galinab@vt.edu>
2017-06-26 11:46:04 -0400

    fixing meter assessment

--- API/Toscanini.gradeLevel.js
+++ API/Toscanini.gradeLevel.js
@@ -11,22 +11,34 @@ const gradeScore = (musicObj) =>
 
     timeSignatures.forEach((timeSignature) =>
     {
-      if ((timeSignature === [4, 4] || timeSignature === [2, 4]
+      if (timeSignature === [7, 8])
+      {
+        meterAssessment.push(5);
+      }
+      else if (timeSignature === [5, 4] || timeSignature === [9, 8]
+        || timeSignature === [12, 8] || timeSignature === [5, 8])
+      {
+        meterAssessment.push(4);
+      }
+      else if (timeSignature === [6, 8] || timeSignature === [6, 4]
+        || timeSignature === [3, 8])
+      {
+        meterAssessment.push(3);
+      }
+      else if ((timeSignature === [2, 2] || timeSignature === [4, 4]
+        || timeSignature === [2, 4] || timeSignature === [3, 4])
+        && timeSignatures.length > 1)
+      {
+        meterAssessment.push(2);
+      }
+      else if ((timeSignature === [4, 4] || timeSignature === [2, 4]
         || timeSignature === [3, 4]) && timeSignatures.length === 1)
-        {
-          //grade level 1
-          meterAssessment.push(1);
-        }
-      else if (timeSignature === [4, 4] || timeSignature === [2, 4]
-        || timeSignature === [3, 4] || timeSignature === [2, 2])
-        {
-
-        }
-      else if (timeSignature === [6, 8] || timeSignature === [2, 4]
-        || timeSignature === [3, 4] || timeSignature === [2, 2])
-        {
-
-        }
+      {
+        meterAssessment.push(1);
+      }
+      else {
+        meterAssessment.push(6);
+      }
     });
   }
 }
