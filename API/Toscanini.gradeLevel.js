9af0232d160fd3576d67e016ad8f981328a5e398
galinabee <galinab@vt.edu>
2017-06-26 16:45:50 -0400

    mysterious second test

--- API/Toscanini.gradeLevel.js
+++ API/Toscanini.gradeLevel.js
@@ -1,16 +1,24 @@
 "use strict";
 const Toscanini = require("./Toscanini");
-const fs = require("fs");
 const path = require("path");
 
-const gradeScore = (xmlfilename) =>
+function arraySame(first, second) {
+    var compareLen = first.length;
+    if (compareLen != second.length) return false;
+    while (compareLen--) {
+        if (first[compareLen] !== second[compareLen]) return false;
+    }
+    return true;
+};
+
+//------------------------------------------------------------------------
+
+const gradeScore = (musicxml) =>
 {
   const gradeLevel = {};
 
   gradeLevel.assessMeter = () =>
   {
-    const musicxml =
-      fs.readFileSync(xmlfilename, "utf-8")
     const toscanini = Toscanini(musicxml);
     const timeSignatures = toscanini.getTimeSignatures();
     const meterAssessment = [];
@@ -18,28 +26,31 @@ const gradeScore = (xmlfilename) =>
 
     timeSignatures.forEach((timeSignature) =>
     {
-      if (timeSignature = [7, 8])
+      if (arraySame(timeSignature, [7, 8]))
       {
         meterAssessment.push(5);
       }
-      else if (timeSignature == [5, 4] || timeSignature == [9, 8]
-        || timeSignature == [12, 8] || timeSignature == [5, 8])
+      else if (arraySame(timeSignature, [5, 4])
+        || arraySame(timeSignature, [9, 8]) || arraySame(timeSignature, [12, 8])
+        || arraySame(timeSignature, [5, 8]))
       {
         meterAssessment.push(4);
       }
-      else if (timeSignature == [6, 8] || timeSignature == [6, 4]
-        || timeSignature == [3, 8])
+      else if (arraySame(timeSignature, [6, 8]) || arraySame(timeSignature, [6, 4])
+        || arraySame(timeSignature, [3, 8]))
       {
         meterAssessment.push(3);
       }
-      else if ((timeSignature == [2, 2] || timeSignature == [4, 4]
-        || timeSignature == [2, 4] || timeSignature == [3, 4])
-        && timeSignatures.length > 1)
+      else if ((arraySame(timeSignature, [2, 2])
+        || arraySame(timeSignature, [4, 4]) || arraySame(timeSignature, [2, 4])
+        || arraySame(timeSignature, [3, 4]))
+        & timeSignatures.length > 1)
       {
         meterAssessment.push(2);
       }
-      else if ((timeSignature == [4,4] || timeSignature == [2, 4]
-        || timeSignature == [3, 4]) && (timeSignatures.length = 1))
+      else if ((arraySame(timeSignature, [4,4]) ||
+        arraySame(timeSignature, [2, 4]) || arraySame(timeSignature, [3, 4]))
+        & (timeSignatures.length == 1))
       {
         meterAssessment.push(1);
       }
@@ -62,25 +73,13 @@ const gradeScore = (xmlfilename) =>
 };
 
 //======================================================================
-const xml2js = require("xml2js");
-const parser = new xml2js.Parser({explicitArray: false, mergeAttrs: true});
-
+// constructor
 const constructor = (musicxml) =>
 {
-  let toscObj;
-
-  parser.parseString(musicxml, (err, obj) =>
-  {
-    if (err)
-    {
-      throw err;
-    }
-    toscObj = obj;
-  });
-
-  // console.log(JSON.stringify(scoreObj, null, 4));
-
-  return gradeScore(toscObj);
+  return gradeScore(musicxml);
 };
 
 module.exports = constructor;
+
+//10-1:30 = 3:30
+//2:30-4:30 = 2:00
